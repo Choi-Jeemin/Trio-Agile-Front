615bb5229f7affe975be3b8df8727692cb448869
BieNew22 <newbie11004@gmail.com>
2024-07-29 21:34:41 +0900

    Feat: 구매 버튼의 활성화/비활성화 설정 코드 추가

--- Assets/Scripts/Building/HouseController.cs
+++ Assets/Scripts/Building/HouseController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestHous : MonoBehaviour
+public class HouseController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class TestHous : MonoBehaviour
 
     private void OnMouseDown()
     {
-        FindAnyObjectByType<HouseManager>().ClickHouse(gameObject);
+        FindAnyObjectByType<HouseUIManager>().ClickHouse(gameObject);
     }
 }
--- Assets/Scripts/Building/HouseUIManager.cs
+++ Assets/Scripts/Building/HouseUIManager.cs
@@ -1,20 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// House 건물 관리
 /// - 건물 클릭시 팝업창 나타나게 하기
 /// </summary>
-public class HouseManager : MonoBehaviour
+public class HouseUIManager : MonoBehaviour
 {
     public GameObject houseUI;
 
+    public Button buyPown;
+    public Button buyWarrior;
+    public Button buyArcher;
+
+    // 사용자가 선택한 건물.
     private GameObject nowHouse;
+
+    Dictionary<GameObject, bool> buttonStatus = new Dictionary<GameObject, bool>();
 
     private void Start()
     {
-        OpenOrCloseUI();
+        OpenOrCloseUI(true);
     }
 
     /// <summary>
@@ -23,8 +33,13 @@ public class HouseManager : MonoBehaviour
     /// <param name="house">현재 클릭한 House</param>
     public void ClickHouse(GameObject house)
     {
+        if (!buttonStatus.ContainsKey(house))
+        {
+            buttonStatus[house] = true;
+        }
         this.nowHouse = house;
-        OpenOrCloseUI();
+        //OpenOrCloseUI(buttonStatus[this.nowHouse]);
+        OpenOrCloseUI(false);
     }
 
     /// <summary>
@@ -33,14 +48,20 @@ public class HouseManager : MonoBehaviour
     public void ClickOther()
     {
         this.nowHouse = null;
-        OpenOrCloseUI();
+        OpenOrCloseUI(true);
     }
 
     /// <summary>
     /// House에 클릭 여부에 따른 UI 화면 출력 여부 결정.
     /// </summary>
-    private void OpenOrCloseUI()
+    /// <param name="status">구매 Button의 활성화 여부 결정.</param>
+    private void OpenOrCloseUI(bool status)
     {
+        // 해당 화면에 houseUI 표시 여부.
         houseUI.SetActive(nowHouse != null);
+
+        buyArcher.interactable = status;
+        buyPown.interactable = status;
+        buyWarrior.interactable = status;
     }
 }
